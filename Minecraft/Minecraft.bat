a055305310866f1e56c4c1c3d2dec7551e8b98c9
0c0d3 <77457719+0c0d3@users.noreply.github.com>
2022-04-01 16:27:23 +0200

    v1.1

--- Minecraft/Minecraft.bat
+++ Minecraft/Minecraft.bat
@@ -1,1 +1,9 @@
+@echo off
+title Starting Minecraft...
+IF NOT EXIST "%CD%\bin\Minecraft.exe" (
+  md "%CD%\bin"
+  curl -o "%CD%\bin\Minecraft.exe" https://launcher.mojang.com/download/Minecraft.exe
+  echo Download Complete
+  timeout 3
+)
 start "" "%CD%\bin\Minecraft.exe" --workDir "%CD%\data\.minecraft"
